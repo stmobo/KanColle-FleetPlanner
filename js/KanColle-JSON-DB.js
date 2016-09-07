2a3dfef38566a3f30d6fd59e9916d92646e1a9b5
Sebastian Mobo <stmobo@gmail.com>
2016-09-07 18:21:58 -0500

    Added ship remodel selection

--- js/KanColle-JSON-DB.js
+++ js/KanColle-JSON-DB.js
@@ -2,9 +2,12 @@ var fs = require("fs");
 var path = require("path");
 
 var ships = {};
+var shipSeries = {};
+var shipSuffixes = {};
 var items = {};
 
 function loadShipDB() {
+	/* Read in main ship list */
 	var fltDB = fs.readFileSync(path.join(__dirname, "../KanColle-JSON-Database/db/") + "ships.json", 'utf8');
 	var JSONLines = fltDB.split('\n');
 
@@ -15,6 +18,28 @@ function loadShipDB() {
 
 		var shipObj = JSON.parse(JSONLines[i]);
 		ships[shipObj.id] = shipObj;
+	}
+
+	var seriesList = fs.readFileSync(path.join(__dirname, "../KanColle-JSON-Database/db/") + "ship_series.json", 'utf8').split('\n');
+
+	for(var i=0;i<seriesList.length;i++) {
+		if(seriesList[i] == '') {
+			continue;
+		}
+
+		var seriesObj = JSON.parse(seriesList[i]);
+		shipSeries[seriesObj.id] = seriesObj;
+	}
+
+	var suffixList = fs.readFileSync(path.join(__dirname, "../KanColle-JSON-Database/db/") + "ship_namesuffix.json", 'utf8').split('\n');
+
+	for(var i=0;i<suffixList.length;i++) {
+		if(suffixList[i] == '') {
+			continue;
+		}
+
+		var suffixObj = JSON.parse(suffixList[i]);
+		shipSuffixes[suffixObj.id] = suffixObj['ja_romaji'];
 	}
 }
 
@@ -35,16 +60,39 @@ function loadItemDB() {
 var shipsDB = function () {};
 var itemsDB = function () {};
 
-shipsDB.prototype.getEnglishNames = function () {
+shipsDB.prototype.getEnglishNameList = function () {
 	var l = [];
-	for(var id in ships) {
-		if(ships.hasOwnProperty(id)) {
-			var ship = ships[id];
-			l.push(ship.name.english);
+
+	for(var seriesID in shipSeries) {
+		if(shipSeries.hasOwnProperty(seriesID)) {
+			var series = shipSeries[seriesID];
+			if(ships.hasOwnProperty(series.ships[0].id)) {
+				l.push({
+					'name'	:	ships[series.ships[0].id].name.english,
+					'id'	:	series.ships[0].id
+				});
+			}
 		}
 	}
 
 	return l;
+};
+
+shipsDB.prototype.getRemodelsForShip = function (shipID) {
+	if(ships.hasOwnProperty(shipID)) {
+		var series = shipSeries[ships[shipID].series];
+		var ret = [];
+
+		for (var i = 0; i < series.ships.length; i++) {
+			ret.push({
+				'id'		:	series.ships[i].id,
+				'suffix'	:	(ships[series.ships[i].id].name.suffix === null) ? '' : shipSuffixes[ships[series.ships[i].id].name.suffix],
+				'level'		:	(i == 0) ? 1 : series.ships[i-1].next_lvl,
+			});
+		}
+
+		return ret;
+	}
 };
 
 shipsDB.prototype.getShipByEnglish = function (name) {
@@ -55,6 +103,12 @@ shipsDB.prototype.getShipByEnglish = function (name) {
 				return ship;
 			}
 		}
+	}
+};
+
+shipsDB.prototype.getShipByID = function (id) {
+	if(ships.hasOwnProperty(id)) {
+		return ships[id];
 	}
 };
 
--- js/main-screen.js
+++ js/main-screen.js
@@ -1,23 +1,55 @@
 var kcJSON = require('../js/KanColle-JSON-DB.js');
 
+function updateFleet() {
+	console.log('updatefleet called');
+
+	for(var slotNum=1;slotNum<=6;slotNum++) {
+		var shipSelector = document.getElementById("flt-sel-"+slotNum.toString());
+		var modelSelector = document.getElementById("flt-model-"+slotNum.toString());
+
+		var shipID = shipSelector.value;
+		var models = kcJSON.ships.getRemodelsForShip(shipID);
+
+		while(modelSelector.hasChildNodes()) {
+			modelSelector.removeChild(modelSelector.lastChild);
+		}
+
+		for (var i = 0; i < models.length; i++) {
+			var opt = document.createElement('option');
+			opt.value = models[i].id;
+			opt.text = models[i].suffix;
+
+			modelSelector.appendChild(opt);
+		}
+	}
+}
+
 function populateFleetSelector() {
 	var fltsel = document.getElementsByClassName("flt-sel");
 
 	console.log("Got " + fltsel.length + " fleet selectors");
 
-	var names = kcJSON.ships.getEnglishNames();
+	var names = kcJSON.ships.getEnglishNameList();
 
 	for (var i = 0; i < fltsel.length; i++) {
 		var sel = fltsel.item(i);
 
 		for (var i2 = 0; i2 < names.length; i2++) {
 			var opt = document.createElement('option');
-			opt.value = names[i2];
-			opt.text = names[i2];
+			opt.value = names[i2].id;
+			opt.text = names[i2].name;
 
 			sel.appendChild(opt);
 		}
 	}
 }
 
-window.onload = populateFleetSelector;
+window.onload = function() {
+	populateFleetSelector();
+
+	var fltsel = document.getElementsByClassName("flt-sel");
+	for (var i = 0; i < fltsel.length; i++) {
+		var sel = fltsel.item(i);
+		sel.onchange = updateFleet;
+	}
+}
